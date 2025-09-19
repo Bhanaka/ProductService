d46f3af56fa5cb621b5e8205d34284eaeaeeb47f
Bhanaka <bhanaka94pramod@gmail.com>
2025-09-19 12:52:52 +0530

    first commit for productService

---
+++ src/main/java/com/wings/product/ProductServiceApplication.java
@@ -0,0 +1,9 @@
+package com.wings.product;
+import org.springframework.boot.autoconfigure.SpringBootApplication ;
+import org.springframework.boot.SpringApplication ;
+@SpringBootApplication
+public class ProductServiceApplication {
+    public static void main(String [] args){
+        SpringApplication.run(ProductServiceApplication.class ,args) ;
+    }
+}
---
+++ src/main/java/com/wings/product/controllers/ProductBrandController.java
@@ -0,0 +1,18 @@
+package com.wings.product.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("product/brand")
+@Slf4j // for log
+public class ProductBrandController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+}
